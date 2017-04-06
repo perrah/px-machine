74c381643b5e45f7d53e540215a5af59299e4c04
Steward <212335809@logon.ds.ge.com>
2017-04-06 16:28:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# PX Machine
+
+## What is the PX Machine?
+The PX Machine is python code for retrieving data from a sensor HAT on a raspberry pi. the code collects continuous data from the sensors and pushes it to a cloud storage solution on Predix.io (Time Series Service). The python POST's data to an API middleware that handles authentication and connection with the service on predix.io
+
+## What is Predix?
+Predix is GE Digital's Industrial IoT platform for connected devices. Please find more information at http://predix.io
+
+## Getting Started
+
+### Get the source code
+You will need a Pi and a SenseHat (or other sensor)
+Python installed on the Pi
+an internet conneection
+UAA & Timeseries service setup on Predix.io
+
+## Support and Further Information
+
+Ask questions and file tickets on <a href="https://www.predix.io/community" target="_blank">https://www.predix.io/community</a>.
+
+# Copyright
+Copyright &copy; 2015, 2016, 2017 GE Global Research. All rights reserved.
+
+The copyright to the computer software herein is the property of
+GE Global Research. The software may be used and/or copied only
+with the written permission of GE Global Research or in accordance
+with the terms and conditions stipulated in the agreement/contract
+under which the software has been supplied.
---
+++ px-machine.py
@@ -0,0 +1,65 @@
+#imports for code
+import time
+import datetime
+import os
+import json
+import requests
+from sense_hat import SenseHat
+
+#set variables for data uploads
+url = "https://etc-px-api.run.aws-usw02-pr.ice.predix.io/dataPoints"
+
+attributes = {
+    "SysId" : "etcPi",
+    "Region" : "HeathersDesk",
+    "Owner" : "DTLPs"
+}
+
+proxies = {
+    "http" : "3.28.29.241:88",
+    "https" : "3.28.29.241:88"
+}
+
+headers = {
+    "content-type" : "application/json",
+    "x-clientid" : "healthcare_etc-px-api_prod",
+    "x-base64-credentials" : "Basic aGVhbHRoY2FyZV9ldGMtcHgtYXBpX3Byb2Q6VXBUbkV6WFZ0eXlWZ3NBVg==",
+    "cache-control" : "no-cache"
+}
+
+#set sense hat variables
+sense = SenseHat()
+
+# Output temp sensor reading (shared with humidity sensor)
+# Assumes senseHat library already loaded above
+while True:
+
+    timeStamp = int(time.time() * 1000)
+
+    try:
+        temp = sense.get_temperature()
+        temp = round(temp, 1)
+
+        data = {
+            "body": [{
+		"name": "ETC_PX_TRAINING",
+		"datapoints": [
+			[
+				timeStamp,
+				temp,
+				3
+			]
+		],
+		"attributes": attributes
+            }]
+        }#end data json
+
+
+        data_json = json.dumps(data)
+        print(data_json)
+        data_post = requests.post(url, data_json, headers=headers, proxies=proxies)
+
+    except:
+        print("Sensor probably not connected")
+
+    time.sleep(300)
